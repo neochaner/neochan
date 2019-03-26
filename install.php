4232972149480ee1b720fb45e4c2d96060f8effd
neochaner <admin@neochan.net>
2019-03-26 21:36:50 +0300

    fix install

--- install.php
+++ install.php
@@ -5,66 +5,65 @@ if(php_sapi_name() !== 'cli'){
 }
 
 
-$options = getopt("hd:u:p:");
-
-if(isset($options['h'])){
-
-    echo "
-    NEOCHAN - install script
-
-    usage: php install.php
-    
-    Options: 
-            -h  show this message
-            -w  site directory
-            -d  db name
-            -u  db user
-            -p  db password  
-
-    Example:
-            php backup.php --user=root --password=secret --database=blog
-    ";
-    exit;
-}
-
-if(!isset($options['w'],$options['d'], $options['u'], $options['p'])){
+if(count($argv) != 5){
     echo 'Wrong arguments';
-    var_dump();
     exit;
 }
 
-$www_dir = $options['w'];
-$db_name = $options['d'];
-$db_user = $options['u'];
-$db_pass = $options['p'];
+$www_dir = $argv[1];
+$db_name = $argv[2];
+$db_user = $argv[3];
+$db_pass = $argv[4];
 $secrets_template = 'inc/secrets.example.php';
-$secrets_file = 'inc/secrets.test.php';
+$secrets_file = 'inc/secrets.php';
 
 chdir($www_dir); 
 
-include $secrets_template;
 
-$config['db']['database'] = $db_name;
-$config['db']['user'] = $db_user;
-$config['db']['password'] = $db_pass;
+
+ 
 
 message("create rsa keys");
 $keys = generate_keys();
 
-$config['encryption']['key_length'] = 2048;
-$config['encryption']['key_alg'] = 'sha512';
-$config['encryption']['public_key'] =  $keys['public'];
-$config['encryption']['private_key'] =  $keys['private'];
-
-
 message("generate trip/cookie salt");
-$config['secure_trip_salt'] = gen_uuid();
-$config['cookies']['salt'] = gen_uuid();
+$secure_trip_salt = gen_uuid();
+$cookies_salt = gen_uuid();
 
-$var_str = var_export($config, true);
-$var = "<?php\n\n\$config = $var_str;\n\n?>";
 
-file_put_contents($secrets_file, $var);
+file_put_contents($secrets_file, "<?php
+
+\$config['db']['server'] = 'localhost';
+\$config['db']['database'] = '$db_name';
+\$config['db']['prefix'] = '';
+\$config['db']['user'] = '$db_user';
+\$config['db']['password'] = '$db_pass';
+
+\$config['fat_system'] = false;
+\$config['fat_size'] = 10 * 1024 * 1024;
+\$config['fat_server'] = 'http://s.site.ru';
+\$config['fat_ftpip'] = '15.18.183.3';
+\$config['fat_ftpuser'] = 'ftpuser';
+\$config['fat_ftppass'] = 'ftppass';
+\$config['fat_ftp_timeout'] = 5;
+
+// defaul timezone
+\$config['timezone'] = 'UTC';
+// defaul cache
+\$config['cache']['enabled'] = 'memcached';
+
+
+\$config['secure_trip_salt'] = '$secure_trip_salt';
+\$config['cookies']['salt'] = '$cookies_salt';
+
+
+\$config['encryption']['key_length'] = 2048;
+\$config['encryption']['key_alg'] = 'sha512';
+\$config['encryption']['public_key'] = '{$keys['public']}';
+\$config['encryption']['private_key'] = '{$keys['private']}';
+
+
+?>");
 chmod($secrets_file, 0444);
 
 
